55beace654045f43435de984e932bea2a3a3660a
Joshua <joshua.althueser@gmail.com>
2024-09-24 15:08:28 +0200

    Second rise text
    uni
    
    data
    
    data
    
    home
    
    data

--- sources/Continuum/ModelInitializer.cpp
+++ sources/Continuum/ModelInitializer.cpp
@@ -3,6 +3,10 @@
 #include <Utility/ConstexprPower.hpp>
 
 namespace Continuum {
+	c_float ModelInitializer::compute_screening() const
+	{
+		return screening_ratio * PhysicalConstants::screening_prefactor * sqrt(fermi_wavevector);
+	}
 	c_float ModelInitializer::compute_fermi_energy() const
 	{
 		const c_float kinetic = bare_dispersion(fermi_wavevector);
@@ -16,6 +20,11 @@ namespace Continuum {
 	c_float ModelInitializer::compute_rho_F() const
 	{
 		return fermi_wavevector / (2. * PI * PI);
+		/* const c_float kf2 = fermi_wavevector * fermi_wavevector;
+		const c_float ks2 = screening * screening;
+		return kf2 / (2. * PI * PI * (fermi_wavevector - 0.5 * PhysicalConstants::em_factor * coulomb_scaling * 
+			( 2. * kf2 - ks2 ) * ( std::log(1. + 4. * kf2 / ks2) / kf2 - 4. / (4. * kf2 + ks2) )
+		));*/
 	}
 
 	std::ostream& operator<<(std::ostream& os, ModelInitializer const& init) 
--- sources/Continuum/ModelInitializer.hpp
+++ sources/Continuum/ModelInitializer.hpp
@@ -10,26 +10,30 @@ namespace Continuum {
 		c_float omega_debye;
 		c_float fermi_wavevector;
 		c_float coulomb_scaling;
-		c_float screening;
+		c_float screening_ratio;
 
 		// Members computed depending on set members
+		c_float screening;
 		c_float fermi_energy;
 		c_float rho_F;
 
 		ModelInitializer(Utility::InputFileReader& input)
-			: temperature{ PhysicalConstants::k_B * input.getDouble("T") }, 
+			: temperature{ input.getDouble("T") },
 			phonon_coupling{ input.getDouble("phonon_coupling") },
 			omega_debye{ 1e-3 * input.getDouble("omega_debye") }, // given in meV in the parameter file
 			fermi_wavevector{ input.getDouble("k_F") },
 			coulomb_scaling{ input.getDouble("coulomb_scaling") },
-			screening{ is_zero(coulomb_scaling) ? c_float{} : input.getDouble("screening") },
+			screening_ratio{ is_zero(coulomb_scaling) ? c_float{} : input.getDouble("screening") },
+			screening{ compute_screening() },
 			fermi_energy{ compute_fermi_energy() },
 			rho_F{ compute_rho_F() }
 		{ };
 
+		c_float compute_screening() const;
 		c_float compute_fermi_energy() const;
 		c_float compute_rho_F() const;
 		inline void recompute_dependencies() {
+			this->screening = compute_screening();
 			this->fermi_energy = compute_fermi_energy();
 			this->rho_F = compute_rho_F();
 		}
